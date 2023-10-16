e4e98d495e8fbfde9c2c221af8df67342cea48c0
kwanhyo <lhc2472@gmail.com>
2023-10-16 22:17:53 +0900

    project2 Create

--- src/App.js
+++ src/App.js
@@ -3,12 +3,47 @@ import './App.css';
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
+import { useState, useRef } from "react"
+
+const mocTodo = [
+  {
+    id:0,
+    isDone:false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id:1,
+    isDone:false,
+    content: "빨래 널기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id:2,
+    isDone:false,
+    content: "노래 연습하기",
+    createdDate: new Date().getTime(),
+  },
+]
 
 function App() {
+  const [todo, setTodo] = useState(mocTodo);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newItem = {
+      id:idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  }
   return (
   <div className="App">
     <Header />
-    <TodoEditor />
+    <TodoEditor onCreate={onCreate}/>
     <TodoList />
   </div>
   )
