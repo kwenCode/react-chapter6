7928f214d3bbc9e24e82ae4b6add8d4a3b51cab5
kwanhyo <lhc2472@gmail.com>
2023-10-20 22:15:30 +0900

    useReducer test

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
-import { useState, useRef } from "react"
+//import TestComp from "./component/TestComp"
+import { useReducer, useRef } from "react"
 
-const mokTodo = [
+const mockTodo = [
   {
     id:0,
     isDone:false,
@@ -26,41 +27,58 @@ const mokTodo = [
   },
 ]
 
+const reducer = (state, action) => {
+  switch(action.type){
+    case "CREATE": {
+      return [action.newItem, ...state];
+    }
+    case "UPDATE":
+      return state.map((it)=> 
+      it.id === action.targetId 
+      ? {
+        ...it,
+        isDone: !it.isDone,
+      }
+      : it
+    );
+    case "DELETE":{
+      return state.filter((it)=> it.id !== action.targetId);
+    }
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todo, setTodo] = useState(mokTodo);
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newItem = {
-      id:idRef.current,
-      content,
-      isDone: false,
-      createdDate: new Date().getTime(),
-    };
-    setTodo([newItem, ...todo]);
+    dispatch({
+      type:"CREATE",
+      newItem: {
+        id: idRef.current,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
     idRef.current += 1;
   }
   
   const onUpdate = (targetId) => {
-    setTodo(
-      todo.map(
-        (it) => {
-          if(it.id === targetId){
-            return {
-              ...it,
-              isDone: !it.isDone,
-            };
-          }else {
-            return it;
-          }
-        }
-      )
-    )
+    dispatch({
+      type:"UPDATE",
+      targetId,
+    });
   }
 
   const onDelete = (targetId) =>{
-    setTodo(todo.filter((it)=> it.id !== targetId))
-  }
+    dispatch({
+      type:"DELETE",
+      targetId,
+    });
+  };
   return (
   <div className="App">
     <Header />
